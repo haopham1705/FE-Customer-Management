f6b979810ccb4d06284270f6170573ef06fc7690
haopham1705 <phamvanhao009@gmail.com>
2021-11-11 15:52:02 +0700

    edit baseURL

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1> React App</h1>
       <p>Abc content website</p>
     </div>
--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:3000/api/',
+    baseURL: 'http://localhost:3001/api/',
     headers: {
         'Content-Type': 'application/json'
     }
