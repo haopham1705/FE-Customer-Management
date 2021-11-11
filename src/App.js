978f16548effd896fdf8c2ef93fe6275b1dac617
Hao Pham Van <phamhao@HawMacintosh.local>
2021-11-11 11:08:12 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+ import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> React App</h1>
+      <p>Abc content website</p>
+    </div>
+  );
+}
+
+export default App;
